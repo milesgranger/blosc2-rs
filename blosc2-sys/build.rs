e26c9370304577c3d1307545fc9eb55999bd0ea4
Miles <miles59923@gmail.com>
2023-04-10 12:36:36 +0200

    Fix Windows build (#3)
    * Specific no default and opaque type attempt
    
    * Allow enums repr themselves
    
    * OSX one thread, Windows GNU
    
    * Trial MinGW
    
    * Use ctor to call blosc2 init/destory in testing

--- blosc2-sys/build.rs
+++ blosc2-sys/build.rs
@@ -17,9 +17,12 @@ fn main() {
     println!("cargo:rustc-link-search={}/lib", lib.display());
     println!("cargo:rustc-link-lib=static=blosc2");
 
-    let out = PathBuf::from(&(format!("{}/src/ffi.rs", env!("CARGO_MANIFEST_DIR"))));
+    let out = PathBuf::from(&(format!("{}/bindings.rs", std::env::var("OUT_DIR").unwrap())));
     bindgen::Builder::default()
         .header(&format!("{}/include/blosc2.h", lib.display()))
+        .layout_tests(false)
+        .no_default("tagMONITORINFOEXA") // Windows specific, no default [u8;40usize]
+        .opaque_type("_IMAGE_TLS_DIRECTORY64") // Windows specific, error[E0588]: packed type cannot transitively contain a #[repr(align)] type
         .derive_default(true)
         .derive_copy(true)
         .derive_debug(true)

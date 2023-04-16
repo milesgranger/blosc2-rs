3d1068213d3ba2fbc9bd1b8a0a2395bd6979e78f
Miles Granger <miles59923@gmail.com>
2023-04-16 08:52:29 +0200

    Add throughput to compress benchmarks [skip ci]

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,25 +1,49 @@
 use blosc2::{compress, decompress, destroy, init, CParams, DParams};
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use std::fs;
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use std::{ffi::OsString, fs};
 
 fn criterion_benchmark(c: &mut Criterion) {
     init();
-    fs::read_dir(format!("{}/data", env!("CARGO_MANIFEST_DIR")))
-        .unwrap()
-        .filter(|f| {
-            f.as_ref()
-                .ok()
-                .map(|f| f.file_type().unwrap().is_file())
-                .is_some()
-        })
-        .for_each(|f| {
-            let name = f.as_ref().unwrap().file_name().to_owned();
-            let data = fs::read(f.unwrap().path()).unwrap();
-            c.bench_function(
-                &format!("compress - defaults - {}", name.to_str().unwrap()),
-                |b| b.iter(|| compress(black_box(&data), None, None, None)),
-            );
+
+    let data: Vec<(OsString, Vec<u8>)> =
+        fs::read_dir(format!("{}/data", env!("CARGO_MANIFEST_DIR")))
+            .unwrap()
+            .filter(|f| {
+                f.as_ref()
+                    .ok()
+                    .map(|f| f.file_type().unwrap().is_file())
+                    .is_some()
+            })
+            .map(|f| {
+                let name = f.as_ref().unwrap().file_name().to_owned();
+                let data = fs::read(f.unwrap().path()).unwrap();
+                (name, data)
+            })
+            .collect();
+
+    let mut compression_group = c.benchmark_group("compress");
+
+    // Compress all data, then file-by-file
+    let all_data = data
+        .iter()
+        .map(|(_, d)| d.clone())
+        .flatten()
+        .collect::<Vec<u8>>();
+    compression_group.throughput(Throughput::Bytes(all_data.len() as _));
+    compression_group.bench_function("defaults - all-data", |b| {
+        b.iter(|| compress(black_box(&all_data), None, None, None))
+    });
+    data.iter().for_each(|(name, data)| {
+        compression_group.throughput(Throughput::Bytes(data.len() as _));
+        compression_group.bench_function(&format!("defaults - {}", name.to_str().unwrap()), |b| {
+            b.iter(|| compress(black_box(&data), None, None, None))
         });
+    });
+
+    // TODO: Decompress all data, then file-by-file
+
+    // TODO: Roundtrip all data, then file-by-file
+
     destroy();
 }
 

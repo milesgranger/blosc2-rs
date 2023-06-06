1d24f9e056c042bbc8131e72be1cab8093408048
Miles Granger <miles59923@gmail.com>
2023-06-06 07:08:41 +0200

    Fixup

--- src/lib.rs
+++ src/lib.rs
@@ -960,6 +960,9 @@ pub mod schunk {
             if self.is_empty() {
                 return Ok(vec![]);
             }
+
+            unsafe { ffi::blosc2_schunk_avoid_cframe_free(*self.0.read(), true) };
+
             let mut needs_free = true;
             let mut ptr: *mut u8 = std::ptr::null_mut();
             let len =

89cceef82cc1d9863f21810b152321dcbfd6b422
Miles Granger <miles59923@gmail.com>
2024-04-02 12:40:32 +0200

    upgrade

--- blosc2-sys/build.rs
+++ blosc2-sys/build.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
 #[cfg(feature = "use-system-blosc2")]
-const BLOSC2_VERSION: &'static str = "2.13.2";
+const BLOSC2_VERSION: &'static str = "2.14.0";
 
 fn main() {
     let header = {
@@ -23,7 +23,7 @@ fn main() {
 
             println!("cargo:rustc-link-search={}/lib64", lib.display());
             println!("cargo:rustc-link-search={}/lib", lib.display());
-            println!("cargo:rustc-link-lib=static=blosc2");
+            println!("cargo:rustc-link-lib=blosc2");
             format!("{}/include/blosc2.h", lib.display())
         }
 

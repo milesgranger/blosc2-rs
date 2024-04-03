73931aa0da2a59b255a035955ac2cbdcef369dc3
Miles Granger <miles59923@gmail.com>
2024-04-03 22:05:15 +0200

    Initial refactor to use process cmake directly

--- blosc2-sys/build.rs
+++ blosc2-sys/build.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+use std::process::Command;
 
 #[cfg(feature = "use-system-blosc2")]
 const BLOSC2_VERSION: &'static str = "2.14.0";
@@ -8,30 +9,51 @@ fn main() {
         // build blosc2 from source
         #[cfg(not(feature = "use-system-blosc2"))]
         {
-            let lib = cmake::Config::new("c-blosc2")
-                .generator("Ninja")
-                .define("BLOSC_INSTALL", "ON")
-                .define("CMAKE_C_FLAGS", "-fPIE")
-                .define("CMAKE_POSITION_INDEPENDENT_CODE", "ON")
-                .define("STATIC_LIB", "ON")
-                .define("SHARED_LIB", "ON")
-                .define("BUILD_TESTS", "OFF")
-                .define("BUILD_EXAMPLES", "OFF")
-                .define("BUILD_SHARED_LIBS", "ON")
-                .define("BUILD_BENCHMARKS", "OFF")
-                .define("BUILD_FUZZERS", "OFF")
-                .define("BUILD_PLUGINS", "OFF")
-                // .profile("Release")
-                // .very_verbose(true)
-                // .pic(true)
-                .out_dir(env!("CARGO_MANIFEST_DIR"))
-                .always_configure(true)
-                .build();
+            let out_dir = std::env::var("OUT_DIR").unwrap();
+            let install_path = format!("{}/{}", &out_dir, "blosc2-install");
+            let build_path = format!("{}/{}", &out_dir, "blosc2-build");
 
-            println!("cargo:rustc-link-search=native={}/lib64", lib.display());
-            println!("cargo:rustc-link-search=native={}/lib", lib.display());
+            let configure_output = Command::new("cmake")
+                .arg(format!("-S{}/{}", env!("CARGO_MANIFEST_DIR"), "c-blosc2"))
+                .arg(format!("-B{}", &build_path))
+                .arg("-DCMAKE_BUILD_TYPE=Release")
+                .arg(format!("-DCMAKE_INSTALL_PREFIX={}", &install_path))
+                .arg("-DCMAKE_C_FLAGS=-fPIE")
+                .arg("-DCMAKE_POSITION_INDEPENDENT_CODE=ON")
+                .arg("-DBUILD_SHARED_LIBS=OFF")
+                .arg("-DBUILD_FUZZERS=OFF")
+                .arg("-DBUILD_BENCHMARKS=OFF")
+                .arg("-DBUILD_SHARED_LIBS=OFF")
+                .arg("-DBUILD_EXAMPLES=OFF")
+                .arg("-DBUILD_TESTS=OFF")
+                .arg("-DBLOSC_INSTALL=ON")
+                .output()
+                .unwrap();
+            if !configure_output.status.success() {
+                panic!(
+                    "{}",
+                    std::str::from_utf8(configure_output.stdout.as_slice()).unwrap()
+                );
+            }
+            let build_output = Command::new("cmake")
+                .arg("--build")
+                .arg(&build_path)
+                .arg("--target")
+                .arg("install")
+                .output()
+                .unwrap();
+
+            if !build_output.status.success() {
+                panic!(
+                    "{}",
+                    std::str::from_utf8(build_output.stderr.as_slice()).unwrap()
+                );
+            }
+
+            println!("cargo:rustc-link-search={}/lib64", &install_path);
+            println!("cargo:rustc-link-search={}/lib", &install_path);
             println!("cargo:rustc-link-lib=static=blosc2");
-            format!("{}/include/blosc2.h", lib.display())
+            format!("{}/include/blosc2.h", &install_path)
         }
 
         // Use system blosc2

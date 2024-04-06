5acd5d06b532bb3875a55364ce56f78f75fff493
Miles Granger <miles59923@gmail.com>
2024-04-06 13:34:55 +0200

    fixup - add static feat and use cmake crate

--- blosc2-sys/build.rs
+++ blosc2-sys/build.rs
@@ -1,5 +1,4 @@
 use std::path::{Path, PathBuf};
-use std::process::Command;
 
 fn main() {
     println!("cargo::rerun-if-changed=build.rs");
@@ -8,49 +7,32 @@ fn main() {
     #[cfg(not(feature = "use-system-blosc2"))]
     {
         let out_dir_str = std::env::var("OUT_DIR").unwrap();
-        let out_dir = Path::new(&out_dir_str);
-        let build_path = out_dir.join("blosc2-build");
 
         let install_path_str =
             std::env::var("BLOSC2_INSTALL_PREFIX").unwrap_or(out_dir_str.to_owned());
         let install_path = Path::new(&install_path_str);
 
-        let configure_output = Command::new("cmake")
-            .arg(format!("-S{}/{}", env!("CARGO_MANIFEST_DIR"), "c-blosc2"))
-            .arg(format!("-B{}", build_path.display()))
-            .arg("-DCMAKE_BUILD_TYPE=Release")
-            .arg(format!("-DCMAKE_INSTALL_PREFIX={}", install_path.display()))
-            .arg("-DCMAKE_POSITION_INDEPENDENT_CODE=ON")
-            .arg("-DBUILD_SHARED_LIBS=ON")
-            .arg("-DBUILD_FUZZERS=OFF")
-            .arg("-DBUILD_BENCHMARKS=OFF")
-            .arg("-DBUILD_EXAMPLES=OFF")
-            .arg("-DBUILD_STATIC=ON")
-            .arg("-DBUILD_SHARED=ON")
-            .arg("-DBUILD_TESTS=OFF")
-            .arg("-DBLOSC_INSTALL=ON")
-            .output()
-            .unwrap();
-        if !configure_output.status.success() {
-            panic!(
-                "{}",
-                std::str::from_utf8(configure_output.stdout.as_slice()).unwrap()
-            );
-        }
-        let build_output = Command::new("cmake")
-            .arg("--build")
-            .arg(&build_path)
-            .arg("--target")
-            .arg("install")
-            .output()
-            .unwrap();
+        let mut cmake_conf = cmake::Config::new("c-blosc2");
+        cmake_conf
+            .define("CMAKE_POSITION_INDEPENDENT_CODE", "ON")
+            .define("BUILD_SHARED_LIBS", "ON")
+            .define("BUILD_FUZZERS", "OFF")
+            .define("BUILD_BENCHMARKS", "OFF")
+            .define("BUILD_EXAMPLES", "OFF")
+            .define("BUILD_STATIC", "ON")
+            .define("BUILD_SHARED", "ON")
+            .define("BUILD_TESTS", "OFF")
+            .define("BUILD_PLUGINS", "OFF")
+            .always_configure(true);
 
-        if !build_output.status.success() {
-            panic!(
-                "{}",
-                std::str::from_utf8(build_output.stderr.as_slice()).unwrap()
-            );
+        if std::env::var("BLOSC2_INSTALL_PREFIX").is_ok() {
+            let install_prefix = format!("{}", install_path.display());
+            cmake_conf
+                .define("CMAKE_INSTALL_PREFIX", install_prefix)
+                .define("BLOSC_INSTALL", "ON");
         }
+
+        cmake_conf.build();
 
         for subdir in &["lib64", "lib", "bin"] {
             let search_path = install_path.join(subdir);
@@ -84,6 +66,10 @@ fn main() {
         }
     }
 
+    #[cfg(feature = "static")]
+    println!("cargo:rustc-link-lib=static=blosc2");
+
+    #[cfg(not(feature = "static"))]
     println!("cargo:rustc-link-lib=blosc2");
 
     let out = PathBuf::from(&(format!("{}/bindings.rs", std::env::var("OUT_DIR").unwrap())));
